2f4e2d06048bb36243e54dd86cc53b5eae971b87
usuario <usuario@10.22.143.60>
2016-11-04 09:37:52 +0000

    Kata4v3

--- src/kata4/HistogramDisplay.java
+++ src/kata4/HistogramDisplay.java
@@ -1,4 +1,4 @@
-package kata3;
+package kata4;
 
 import java.awt.Dimension;
 import javax.swing.JPanel;
@@ -9,32 +9,34 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
-public class HistogramDisplay extends ApplicationFrame { 
+public class HistogramDisplay extends ApplicationFrame {
+
     private final Histogram<String> histogram;
-    
-    public HistogramDisplay (Histogram<String> histogram) {
+
+    public HistogramDisplay(Histogram<String> histogram) {
         super("Ventana");
         this.histogram = histogram;
         setContentPane(createPanel());
         pack();
     }
-        
+
     public void execute() {
         setVisible(true);
     }
-    
+
     private JPanel createPanel() {
         ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
         setPreferredSize(new Dimension(500, 400));
         return chartPanel;
     }
-    
+
     private JFreeChart createChart(DefaultCategoryDataset dataSet) {
-        JFreeChart chart = ChartFactory.createBarChart("JFreeChart Histogram", 
+        JFreeChart chart = ChartFactory.createBarChart("JFreeChart Histogram",
                 "Dominios email", "Nº Emails", dataSet, PlotOrientation.VERTICAL,
                 false, rootPaneCheckingEnabled, rootPaneCheckingEnabled);
         return chart;
     }
+
     private DefaultCategoryDataset createDataset() {
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
         for (String key : histogram.keySet()) {
